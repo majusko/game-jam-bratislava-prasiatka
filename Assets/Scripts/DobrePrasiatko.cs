534e83de2130987de82e5db118f80083749e25a5
Mario Kapusta <mario.kapusta@ubisoft.com>
2020-03-22 00:41:28 +0100

    Edits.

---
+++ Assets/Scripts/DobrePrasiatko.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DobrePrasiatko : MonoBehaviour
+{
+
+    public float amplitude;
+    public float speed;
+    public float rotateSpeed;
+    public float lifetime;
+    public float timeSpentUp;
+    public float speedOfSlideUpAndDown;
+
+    private bool goDown = false;
+    private bool imUp = false;
+
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Transform animalUp = transform.parent.Find("AnimalUp");
+        Transform animalDown = transform.parent.Find("AnimalDown");
+
+
+        if(goDown)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, animalDown.position, speedOfSlideUpAndDown * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, animalDown.position) < 0.1f)
+            {
+                gameObject.SetActive(false);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, animalUp.position, speedOfSlideUpAndDown * Time.deltaTime);
+            if (!imUp && transform.position.Equals(animalUp.position))
+            {
+                imUp = true;
+                Invoke("GoDown", timeSpentUp);
+
+            }
+        }
+
+        //trasenie - ---
+        //transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+        //float AngleAmount = (Mathf.Cos(Time.time * rotateSpeed) * 180) / Mathf.PI * 0.5f;
+        //AngleAmount = Mathf.Clamp(AngleAmount, -15, 15);
+        //transform.localRotation = Quaternion.Euler(0, 0, AngleAmount);
+        //trasenie - ---
+
+    }
+
+    private void GoDown()
+    {
+        goDown = true;
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public GameObject planet;
+    public List<GameObject> planetObjects;
+    public List<GameObject> spawningPigs;
+    public List<GameObject> spawningOctopus;
+    public float spawnSomethingEvery;
+    public float chanceForPig;
+    public float waitTimeForNext;
+
+    private List<GameObject> currentlySpawned;
+    private float nextActionTime = 0.0f;
+    private Dictionary<string, float> closedHrnce = new Dictionary<string, float>();
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += spawnSomethingEvery;
+            SpawnSomething();
+        }
+
+        var removals = new List<string>();
+
+        foreach (KeyValuePair<string, float> entry in closedHrnce) {
+            if(entry.Value < Time.time)
+            {
+                removals.Add(entry.Key);
+            }
+        }
+
+        foreach(string removal in removals)
+        {
+            closedHrnce.Remove(removal);
+        }
+    }
+
+    private void SpawnSomething()
+    {
+        GameObject spawnBehind = NextPlaceToPick();
+
+        GameObject spawningObject;
+
+        if(Random.Range(0f, 1f) < chanceForPig)
+        {
+            spawningObject = spawningPigs[Random.Range(0, spawningPigs.Count)];
+        } else
+        {
+            spawningObject = spawningOctopus[Random.Range(0, spawningOctopus.Count)];
+        }
+
+        GameObject intantiated = Instantiate(spawningObject, spawnBehind.transform.position, spawnBehind.transform.rotation);
+
+        intantiated.transform.SetParent(spawnBehind.transform, true);
+    }
+
+    private GameObject NextPlaceToPick()
+    {
+        GameObject spawnBehind = planetObjects[Random.Range(0, planetObjects.Count)];
+
+        if (closedHrnce.ContainsKey(spawnBehind.name))
+        {
+            return NextPlaceToPick();
+        }
+
+        closedHrnce[spawnBehind.name] = Time.time + waitTimeForNext;
+
+        return spawnBehind;
+    }
+
+}
