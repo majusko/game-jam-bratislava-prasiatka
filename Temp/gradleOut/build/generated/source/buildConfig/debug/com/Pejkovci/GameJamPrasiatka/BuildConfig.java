6f9bfcfc6afd612f27c7077b0bad9955e7943964
Mario Kapusta <mario.kapusta@ubisoft.com>
2020-03-23 01:54:07 +0100

    Fix resolution bug and rebalance.

---
+++ Temp/gradleOut/build/generated/source/buildConfig/debug/com/Pejkovci/GameJamPrasiatka/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.Pejkovci.GameJamPrasiatka;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.Pejkovci.GameJamPrasiatka";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
