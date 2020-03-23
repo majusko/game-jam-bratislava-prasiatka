ff8ac4fe50818b8d367cc2531f4e0470dc229f17
lgubalovie <lackooo@gmail.com>
2020-03-23 14:52:28 +0100

    Uprava Textov v komixe + zmena buttonov

---
+++ Assets/Scripts/LietajuceLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LietajuceLogic : MonoBehaviour
+{
+    public bool somPrasa;
+    private GameObject levelManager;
+    private LevelManager levelManagerScript;
+    public GameObject explotion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManagerScript = levelManager.GetComponent<LevelManager>();
+    }
+
+    // Update is called once per frame
+  
+    void OnMouseDown()
+    {
+        if (somPrasa)
+        {
+            PlayerGlobalState.Instance.lifes -= 1;
+        }
+        else
+        {
+            levelManagerScript.KillChobotnica();
+        }
+
+        GameObject particles = Instantiate(explotion, transform.position, Quaternion.identity) as GameObject;
+
+        Destroy(particles, 2);
+
+        Destroy(gameObject);
+    }
+}
