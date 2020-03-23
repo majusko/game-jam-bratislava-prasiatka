e7b83a8e15fc0d8adb479d76ad7820db59119460
lgubalovie <lackooo@gmail.com>
2020-03-23 16:45:36 +0100

    Audio prefabs / music/loop/single

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    void Update()
+    {
+        Scene m_scene;
+        m_scene = SceneManager.GetActiveScene();
+        if (m_scene.name == "SampleScene")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    private void SetUpSingleton()
+    {
+       
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
