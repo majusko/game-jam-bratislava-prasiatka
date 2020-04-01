2d93b2fa121e44dd1fa72fc94d662551444f97e4
lgubalovie <lackooo@gmail.com>
2020-04-01 19:01:39 +0200

    novy level + waypoints

---
+++ Assets/Scripts/WaypointControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointControl : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] waypoints;
+
+    public float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        Debug.Log("statre");
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,waypoints[waypointIndex].transform.position,moveSpeed * Time.deltaTime);
+
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+
+        if (waypointIndex == waypoints.Length)
+            waypointIndex = 0;
+    }
+
+}
